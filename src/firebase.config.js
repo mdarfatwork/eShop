f8418a8950790e43366f5cd9efab11fb213846f7
mdarfatwork <work.arfat@gmail.com>
2023-09-12 16:57:20 +0530

    Adding Local Env

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDXfiXzPq5eHrvcFpjVdtttGP3raAk9rY8",
-  authDomain: "eshoparfat.firebaseapp.com",
-  projectId: "eshoparfat",
-  storageBucket: "eshoparfat.appspot.com",
-  messagingSenderId: "28645417707",
-  appId: "1:28645417707:web:dd4048edbed9add66b14ae"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
